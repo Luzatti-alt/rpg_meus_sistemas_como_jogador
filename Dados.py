b885b51a01943877b16a7da1d133289c6fb5ac58
Luzatti-alt <lucasguzatti4@gmail.com>
2025-07-09 21:09:32 -0300

    Update Dados.py
    coreção em certas situações

--- Dados.py
+++ Dados.py
@@ -1,15 +1,16 @@
 import random
 class Dados:
-    tipo = input("Digite o dados que será jogado(como d+num): ")
     def d10():
         return random.randint(1,10)
     def d20():
         return random.randint(1,20)
     def d4():
         return random.randint(1,4)
-    if tipo == "d10":
-        print("caiu: ", d10())
-    elif tipo == "d20":
-        print("caiu: ", d20())
-    elif tipo == "d4":
-        print("caiu: ", d4())
+    if __name__ == "__main__":#so roda esta parte se ele for neste aquivo este é o uso de __name == "__main__"
+        tipo = input("Digite o dados que será jogado(como d+num): ")
+        if tipo == "d10":
+            print("caiu: ", d10())
+        elif tipo == "d20":
+            print("caiu: ", d20())
+        elif tipo == "d4":
+            print("caiu: ", d4())
