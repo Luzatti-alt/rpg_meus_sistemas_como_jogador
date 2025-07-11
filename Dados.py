875f883de58c272dcd7a99db5378eda216b7a52f
Luzatti-alt <lucasguzatti4@gmail.com>
2025-07-11 15:46:32 -0300

    Update Dados.py

--- Dados.py
+++ Dados.py
@@ -1,11 +1,13 @@
 import random
 class Dados:
+    def d4():
+        return random.randint(1,4)
+    def d6():
+        return random.randint(1,6)
     def d10():
         return random.randint(1,10)
     def d20():
         return random.randint(1,20)
-    def d4():
-        return random.randint(1,4)
     if __name__ == "__main__":#so roda esta parte se ele for neste aquivo este é o uso de __name == "__main__"
         tipo = input("Digite o dados que será jogado(como d+num): ")
         if tipo == "d10":
