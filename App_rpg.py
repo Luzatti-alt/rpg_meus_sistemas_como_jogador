580d71cf793698cb906dde909a32b4b1e823d1ff
Luzatti-alt <lucasguzatti4@gmail.com>
2025-08-20 15:57:49 -0300

    Update App_rpg.py

--- App_rpg.py
+++ App_rpg.py
@@ -113,10 +113,11 @@ class TelaPrincipal(Screen):
             size=(300, 100),
             pos= (0, altura / 2 + 125)
             )
-        nomes_personagens_lista = session.query(Ficha.nome_personagem).all()
+        nomes_personagens_tuplas = session.query(Ficha.nome_personagem).all()
+        nomes_personagens_lista = [nome[0] for nome in nomes_personagens_tuplas]
         self.personagem = Spinner(
             text='personagem',
-            values=('ver na ficha db',str(nomes_personagens_lista)),
+            values=(nomes_personagens_lista),
             size_hint=(None, None),
             size=(300, 100),
             pos= (0, altura / 2 + 125)
